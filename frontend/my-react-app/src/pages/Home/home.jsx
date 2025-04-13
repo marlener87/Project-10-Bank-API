5e51d1244bc0c21be92a1c448e8579673ebae14f
marlener87 <53823616+marlener87@users.noreply.github.com>
2025-04-13 18:52:26 +0200

    un peu de CSS pour finir

--- frontend/my-react-app/src/pages/Home/home.jsx
+++ frontend/my-react-app/src/pages/Home/home.jsx
@@ -26,17 +26,17 @@ const Home = () => {
                 <CardHome 
                     title={'You are our #1 priority'} 
                     icon={iconChat}>
-                    <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
+                    <p className='pSection'>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
                 </CardHome>
                 <CardHome 
                     title={'More savings means higher rates'}
                     icon={'/assets/icon-money.png'}>
-                    <p>The more you save with us, the higher your interest rate will be!</p>
+                    <p className='pSection'>The more you save with us, the higher your interest rate will be!</p>
                 </CardHome>
                 <CardHome 
                     title={'Security you can trust'}
                     icon={iconSecurity}>
-                    <p>We use top of the line encryption to make sure your data and money is always safe.</p>
+                    <p className='pSection'>We use top of the line encryption to make sure your data and money is always safe.</p>
                 </CardHome>
             </section>
         </main>
